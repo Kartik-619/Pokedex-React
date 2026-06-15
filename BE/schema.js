17d4e44f348e6c8b5b0930114c349fa2dffe8dbf
Kartik Sharma <poojan4604@gmail.com>
2026-06-15 17:29:23 +0530

    graphQL server setup
    server setup for graphQL,
    defined the schema and standalone server
    modification in resolver needed

---
+++ BE/schema.js
@@ -0,0 +1,23 @@
+import { gql } from 'graphql-tag';
+//we are defining custom objects in here under graphQL and we are defining what that obj would look like and the attributes it might contain
+export const typeDefs=gql`
+
+type Pokemon{
+    id:ID!
+    name:String!
+    height:Int
+    weight:Int
+    sprites:PokemonSprites
+    }
+
+    type PokemonSprites{
+    front_default:String
+    }
+
+    type Query {
+    pokemon(nameOrId: String!): Pokemon
+  }`;
+
+
+//query defines the entry points to a graph and also defines the exit point
+
---
+++ BE/server.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone"
+import express from 'express';
+import { typeDefs } from "./schema.js";
+
+//resolver tells how to a query will be processed for a graph
+const resolvers={
+    Query:{
+        pokemon:async (_,{nameOrId})=>{
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrId.toLowerCase()}`);
+            const data = await response.json();
+      
+            // 2. Return the data mapped to our Schema
+            return {
+              id: data.id,
+              name: data.name,
+              height: data.height,
+              weight: data.weight,
+              sprites: {
+                front_default: data.sprites.front_default
+              }
+            };
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,   // Must be plural 'typeDefs' to match Apollo's expectations
+    resolvers,
+  });
+  
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.log("server running at port 4000")
